08336028fbaaebef6dac75243240ef2c622481eb
mandarynchik <95445536+mandarynchik@users.noreply.github.com>
2022-01-11 20:17:03 +0200

    Simple Telegram Bot 1.0

--- 1_base_funcs/telegram_bot.py
+++ 1_base_funcs/telegram_bot.py
@@ -1,35 +1,26 @@
 import requests
 
-my_token = "1818081328:AAFAPDBGdpbhEN5qEgeI6uzaTZFxmaRxyGs"
-root_url = "https://api.telegram.org/bot" 
-get_me = "/getMe"
-updates_endpoint = "/getUpdates"
+# step1 collection of inspirational quotes
+
+
+inspirational_quotes = ["Be yourself; everyone else is already taken.― Oscar Wilde",
+                        "You've gotta dance like there's nobody watching, Love like you'll never be hurt,Sing like there's nobody listening,And live like it's heaven on earth.― William W. Purkey",
+                        "Be the change that you wish to see in the world. ― Mahatma Gandhi"
+                        "Without music, life would be a mistake.― Friedrich Nietzsche, Twilight of the Idols"
+                        "I have not failed. I've just found 10,000 ways that won't work.― Thomas A. Edison"
+                        ]
+# step2 send message
+
+token = "5013724612:AAEoqRNEn4Ue3RGiyTqAJn7ZKXlPQE-IjiM"
+root_url = "https://api.telegram.org/bot"
 message_endpoint = "/sendMessage"
-
-OK_CODES = (200,201,202,203,204,205)
-
-def get_updates(token):
-	updates_url = f"{root_url}{token}{updates_endpoint}"
-
-	res = requests.get(updates_url)
-	if res.status_code in OK_CODES:
-		result = res.json()
-		return result
-	else:
-		print(f"Неудача с запросом: статус {res.status_code}")
+chat_id = "468282070"
+text_message = inspirational_quotes[0]
 
 
 def send_message(chat_id, text_message, token):
-	send_message_url = f"{root_url}{token}{message_endpoint}"
-	res = requests.post(send_message_url, data={"chat_id":chat_id, "text":text_message})
-	if res.status_code in OK_CODES:
-		return True
-	else:
-		print(f"Не удалось послать сообщение - ошибка с кодом {res.status_code}")
+    send_message_url = f"{root_url}{token}{message_endpoint}"
+    res = requests.post(send_message_url, data={"chat_id": chat_id, "text": text_message})
 
 
-
-updates = get_updates(my_token)
-chat_id = updates["result"][-1]["message"]["chat"]["id"]  
-message = "Hello!"
-send_message(chat_id, message, my_token)
+send_message(chat_id, text_message, token)
