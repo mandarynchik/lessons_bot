092eabebd9a40f7bf823183a2e510c3b9b4ab574
mandarynchik <95445536+mandarynchik@users.noreply.github.com>
2022-01-18 02:20:20 +0200

    telegram_bot2.0

--- 1_base_funcs/telegram_bot.py
+++ 1_base_funcs/telegram_bot.py
@@ -1,12 +1,15 @@
 import requests
+import random
+import schedule
+import time
 
 # step1 collection of inspirational quotes
 
 
 inspirational_quotes = ["Be yourself; everyone else is already taken.― Oscar Wilde",
                         "You've gotta dance like there's nobody watching, Love like you'll never be hurt,Sing like there's nobody listening,And live like it's heaven on earth.― William W. Purkey",
-                        "Be the change that you wish to see in the world. ― Mahatma Gandhi"
-                        "Without music, life would be a mistake.― Friedrich Nietzsche, Twilight of the Idols"
+                        "Be the change that you wish to see in the world. ― Mahatma Gandhi",
+                        "Without music, life would be a mistake.― Friedrich Nietzsche, Twilight of the Idols",
                         "I have not failed. I've just found 10,000 ways that won't work.― Thomas A. Edison"
                         ]
 # step2 send message
@@ -15,12 +18,17 @@ token = "5013724612:AAEoqRNEn4Ue3RGiyTqAJn7ZKXlPQE-IjiM"
 root_url = "https://api.telegram.org/bot"
 message_endpoint = "/sendMessage"
 chat_id = "468282070"
-text_message = inspirational_quotes[0]
 
 
-def send_message(chat_id, text_message, token):
+def send_message():
+    text_message = inspirational_quotes[random.randint(0, len(inspirational_quotes)-1)]
     send_message_url = f"{root_url}{token}{message_endpoint}"
-    res = requests.post(send_message_url, data={"chat_id": chat_id, "text": text_message})
+    requests.post(send_message_url, data={"chat_id": chat_id, "text": text_message})
 
 
-send_message(chat_id, text_message, token)
+# send_message()
+schedule.every(3).minutes.do(send_message)
+
+while True:
+    schedule.run_pending()
+    time.sleep(2)
